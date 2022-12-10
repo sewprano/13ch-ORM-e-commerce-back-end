936174f09a399f57ec0f31b8f14500c561e91470
Deborah Wade <sopranosamurai@yahoo.com>
2022-12-09 21:39:05 -0800

    finished find all and find one routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,42 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      attributes: ['id', 'category_name'],
+      include: {
+        model: Product,
+        attributes: ['product_name']
+      }
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: {
+        model: Product,
+        attributes: ['product_name']
+      }
+    });
+    if (!category) {
+      res.status(404).json({message: 'Could not find category with that id'});
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
