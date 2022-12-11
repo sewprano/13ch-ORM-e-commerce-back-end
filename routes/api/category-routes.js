4a7575dda8e5e44d15df05e85d0395d0c6408bbc
Deborah Wade <sopranosamurai@yahoo.com>
2022-12-11 13:56:48 -0800

    Finished Category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,16 +41,47 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).send(updateCategory);
+  } catch (err) {
+    res.status(400).json(err);
+ }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!deleteCategory) {
+      res.status(404).json({message: 'Could not find category with this id'});
+      return;
+    }
+    res.status(200).json(deleteCategory);
+  } catch (err) {
+    res.status(500),json(err);
+  }
 });
 
 module.exports = router;
